1d7310b2cf9c135e2caafd16761a61990cb4d869
AKNAYKHAN <ayxandmirciyev@gmail.com>
2026-05-23 18:25:00 +0000

    Add CIDR to subnet mask converter

--- network_security/2x00_network_fundamentals/4-cidr2mask.sh
+++ network_security/2x00_network_fundamentals/4-cidr2mask.sh
@@ -1,20 +1,2 @@
 #!/bin/bash
-
-n=$1
-mask=""
-
-for i in 1 2 3 4
-do
-    if [ $n -ge 8 ]; then
-        mask+=255
-        n=$((n-8))
-    else
-        val=$((256 - 2**(8-n)))
-        mask+=$val
-        n=0
-    fi
-
-    [ $i -lt 4 ] && mask+="."
-done
-
-echo "$mask"
+n=$1; m=$((n/8)); r=$((n%8)); for i in 1 2 3 4; do [ $i -le $m ] && echo -n 255 || [ $i -eq $((m+1)) ] && echo -n $((256 - 2**(8-r))) || echo -n 0; [ $i -lt 4 ] && echo -n .; done; echo
