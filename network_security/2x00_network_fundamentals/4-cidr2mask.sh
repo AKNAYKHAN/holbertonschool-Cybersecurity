ac150fdc5ee0dfcb4b6e6c56b35663f359338d72
AKNAYKHAN <ayxandmirciyev@gmail.com>
2026-05-23 18:34:01 +0000

    Add CIDR to subnet mask converter

--- network_security/2x00_network_fundamentals/4-cidr2mask.sh
+++ network_security/2x00_network_fundamentals/4-cidr2mask.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-n=$1
-python3 - << END
-n=$n; mask=[255 if n>=8 else (256-2**(8-n) if n>0 else 0) for n in [n, n-8, n-16, n-24]]; print(".".join(map(str, mask)))
-END
+m=$((0xFFFFFFFF ^ ((1 << (32 - $1)) - 1))); printf "%s" "$(( (m >> 24) & 255 )).$(( (m >> 16) & 255 )).$(( (m >> 8) & 255 )).$(( m & 255 ))"
