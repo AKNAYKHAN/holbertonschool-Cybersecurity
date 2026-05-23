f827888bdd52dc8fea5a365ddca1334c859db21f
AKNAYKHAN <ayxandmirciyev@gmail.com>
2026-05-23 18:22:21 +0000

    Add IP to binary converter

--- network_security/2x00_network_fundamentals/3-ip2bin.sh
+++ network_security/2x00_network_fundamentals/3-ip2bin.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-
-ip=$1
-
-for octet in $(echo $ip | tr "." " ")
-do
-    bin=$(echo "obase=2; $octet" | bc)
-    printf "%08d" "$bin"
-    echo -n "."
-done | sed 's/\.$//'
+echo "$1" | awk -F. '{for(i=1;i<=4;i++){printf "%08d", strtonum("0b" dec2bin($i)); if(i<4) printf "."}} function dec2bin(n,r){r=""; while(n>0){r=(n%2)r; n=int(n/2)} return r}'
